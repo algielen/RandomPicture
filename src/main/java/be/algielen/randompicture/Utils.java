b94f575f3ae38a9367cfffafb33d66b16834f97b
Alexandre Gielen <alexandre.gielen@student.vinci.be>
2018-04-01 20:32:30 +0200

    Remove Apache commons + useless class

--- src/main/java/be/algielen/randompicture/Utils.java
+++ src/main/java/be/algielen/randompicture/Utils.java
@@ -1,46 +1,11 @@
 package be.algielen.randompicture;
 
-import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 
 public class Utils {
-
-	public static BufferedImage scale(BufferedImage sbi, double maxWidth,
-									  double maxHeight) {
-		BufferedImage dbi = null;
-		if (sbi != null) {
-			double fWidth = 1;
-			double fHeight = 1;
-			if (sbi.getWidth() > maxWidth) {
-				fWidth = maxWidth / sbi.getWidth();
-			}
-			if (sbi.getHeight() > maxHeight) {
-				fHeight = maxHeight / sbi.getHeight();
-			}
-			if ((fHeight < fWidth && fHeight != 0) || fWidth == 0) {
-				fWidth = fHeight;
-			} else {
-				fHeight = fWidth;
-			}
-			int dWidth = (int) (sbi.getWidth() * fWidth);
-			int dHeight = (int) (sbi.getHeight() * fHeight);
-
-			dbi = new BufferedImage(dWidth, dHeight, sbi.getType());
-			Graphics2D g = dbi.createGraphics();
-			AffineTransform at = AffineTransform.getScaleInstance(fWidth,
-					fHeight);
-			g.drawRenderedImage(sbi, at);
-			System.out.println(String.format("Converted from %dx%d to %dx%d",
-					sbi.getWidth(), sbi.getHeight(), dWidth, dHeight));
-		}
-		return dbi;
-	}
-
 	public static String getFileExtension(File file) {
 		String extension = "";
 		String fileName = file.getName();
