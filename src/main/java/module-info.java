f037a9631234bf85dcade1185392c678e433c94c
Alexandre Gielen <3091507+algielen@users.noreply.github.com>
2020-12-09 18:15:47 +0100

    Add jlink and jpackage scripts and update deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module randompicture {
 	requires java.base;
-	requires javafx.controls;
-	requires javafx.graphics;
 	requires java.desktop;
+	requires javafx.graphics;
+	requires javafx.controls;
 
 	opens be.algielen.randompicture.gui to javafx.graphics; // javafx needs reflection
 }
